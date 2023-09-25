e24406197f904bd8791273a49c33653cf8858e9d
libmartinito <lib.martinito@pm.me>
2023-09-25 16:26:44 +0800

    CC-754 added compiled files

---
+++ solutions/javascript/01-connect-to-port/code/app/main.js
@@ -0,0 +1,10 @@
+const net = require("net");
+
+const server = net.createServer((socket) => {
+  socket.on("close", () => {
+    socket.end();
+    server.close();
+  })
+});
+
+server.listen(4221, "localhost");
