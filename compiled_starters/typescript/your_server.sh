6f2a5db0e020c258c8512c87d1edce032ac7e9a8
Ryan Gang <ryan-gg@outlook.com>
2024-04-19 13:56:37 +0530

    feat: remove deno cache from spawn script

--- compiled_starters/typescript/your_server.sh
+++ compiled_starters/typescript/your_server.sh
@@ -5,5 +5,4 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-deno cache app/main.ts
 exec deno run --allow-net --allow-read --allow-write app/main.ts "$@"
