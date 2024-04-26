1408e467b043947703c74b9786034380286391ce
Ryan Gang <ryan-gg@outlook.com>
2024-04-26 14:51:17 +0530

    style: improve main.ts

--- starter_templates/typescript/app/main.ts
+++ starter_templates/typescript/app/main.ts
@@ -1,11 +1,11 @@
 import * as net from 'net';
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 const server = net.createServer((socket) => {
     socket.end();
 });
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 // server.listen(4221, 'localhost', () => {
