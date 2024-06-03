692f08d6587a91139d649c963d7983b8d632e5aa
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-06-03 13:16:33 +0530

    Add Zig support for the HTTP challenge

---
+++ compiled_starters/zig/src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+// Uncomment this block to pass the first stage
+// const net = std.net;
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    try stdout.print("Logs from your program will appear here!\n", .{});
+
+    // Uncomment this block to pass the first stage
+    // const address = try net.Address.resolveIp("127.0.0.1", 4221);
+
+    // var listener = try address.listen(.{
+    //     .reuse_address = true,
+    // });
+    // defer listener.deinit();
+
+    // const connection = try listener.accept();
+    // try stdout.print("accepted new connection", .{});
+    // connection.stream.close();
+}
