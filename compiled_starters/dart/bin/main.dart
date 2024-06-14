522c73a88beac8dc3cebe293024b214a2fa49404
Danny Tuppeny <danny@tuppeny.com>
2024-06-14 11:39:49 +0100

    Add missing print to templates

--- compiled_starters/dart/bin/main.dart
+++ compiled_starters/dart/bin/main.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 void main() async {
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  print("Logs from your program will appear here!");
+
   // Uncomment this to pass the first stage
   // var server = await HttpServer.bind(InternetAddress.anyIPv6, 4221);
   // await server.forEach((HttpRequest request) {
