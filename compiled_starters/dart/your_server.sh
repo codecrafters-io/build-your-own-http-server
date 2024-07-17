683a2131c4e9e450a7bc35ccf70d73918b1941db
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-17 15:47:04 +0100

    update dart

--- compiled_starters/dart/your_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# DON'T EDIT THIS!
-#
-# CodeCrafters uses this file to test your code. Don't make any changes here!
-#
-# DON'T EDIT THIS!
-dart compile exe bin/main.dart -o compiled
-exec ./compiled "$@"
