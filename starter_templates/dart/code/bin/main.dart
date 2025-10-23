75e148fa7ea39a2ef5a512228bcdc1e6f00cb9a1
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-10-24 03:22:02 +0900

    CC-1984: Change uncomment code text (3rd try)

--- starter_templates/dart/code/bin/main.dart
+++ starter_templates/dart/code/bin/main.dart
@@ -4,7 +4,7 @@ void main() async {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   print("Logs from your program will appear here!");
 
-  // Uncomment this to pass the first stage
+  // TODO: Uncomment the code below to pass the first stage
   //
   // var serverSocket = await ServerSocket.bind('0.0.0.0', 4221);
   //
