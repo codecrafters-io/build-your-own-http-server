cf1c666b3f562ebc16bffb70ffb6a3a83f8bec21
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-05-05 17:25:03 +0900

    Upgrade Zig version to 0.16.0 in configuration files and documentation. Add Dockerfile for Zig 0.16.0.

--- solutions/zig/01-at4/code/src/main.zig
+++ solutions/zig/01-at4/code/src/main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
-const net = std.net;
 
-pub fn main() !void {
-    const address = try net.Address.resolveIp("127.0.0.1", 4221);
-    var listener = try address.listen(.{
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    const address = try std.Io.net.IpAddress.parseIp4("127.0.0.1", 4221);
+    var server = try address.listen(io, .{
         .reuse_address = true,
     });
-    defer listener.deinit();
+    defer server.deinit(io);
 
-    _ = try listener.accept();
+    _ = try server.accept(io);
     std.debug.print("client connected!", .{});
 }
